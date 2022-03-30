f3372d3b53d805efe9754421182987e9ef9b5667
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-03-30 18:07:48 -0300

    get e set

--- oo/conta.py
+++ oo/conta.py
@@ -20,3 +20,18 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_numero(self):
+        return self.__numero
+
+    def get_titular(self):
+        return self.__titular
+    
+    def get_saldo(self):
+        return self.__saldo
+    
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
