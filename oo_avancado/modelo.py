5857e717e2dde91846f7ff9781554aaad0e60abf
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-03-31 17:13:12 -0300

    corrigindo duplicação de codigo

--- oo_avancado/modelo.py
+++ oo_avancado/modelo.py
@@ -1,47 +1,37 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    def dar_like(self):
+        self._likes += 1
+    
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def likes(self):
+        return self._likes
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    def dar_like(self):
-        self.__likes += 1
-    
-    @property
-    def nome(self):
-        return self.__nome
-
-    @property
-    def likes(self):
-        return self.__likes
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+      
 
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
 
-    def dar_like(self):
-        self.__likes += 1
-    
-    @property
-    def nome(self):
-        return self.__nome
-
-    @property
-    def likes(self):
-        return self.__likes
-    
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
