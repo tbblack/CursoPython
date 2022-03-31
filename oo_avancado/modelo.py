4fb6bedb3527402173ac37a3cbe9a8240a3b9a09
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-03-31 18:11:39 -0300

    Estudando herança

--- oo_avancado/modelo.py
+++ oo_avancado/modelo.py
@@ -4,43 +4,39 @@ class Programa:
         self.ano = ano
         self._likes = 0
 
-    def dar_like(self):
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
         self._likes += 1
-    
+
     @property
     def nome(self):
         return self._nome
 
-    @property
-    def likes(self):
-        return self._likes
-    
     @nome.setter
-    def nome(self, novo_nome):
-        self._nome = novo_nome.title()
-
+    def nome(self, nome):
+        self._nome = nome
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
-      
-
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-
-
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f'- Temporadas: {vingadores.duracao} - Likes: {vingadores.likes}')
-
 atlanta = Serie('atlanta', 2018, 2)
-atlanta.dar_like()
-atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-      f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
