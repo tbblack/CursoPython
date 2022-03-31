2b726ac7e14b3f2c19305dc1045f8fa4edbabdb3
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-03-31 16:24:11 -0300

    adicionando atributos e metodos

--- oo_avancado/modelo.py
+++ oo_avancado/modelo.py
@@ -1,20 +1,33 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+    
+
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f'- Temporadas: {vingadores.duracao}')
+      f'- Temporadas: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
+atlanta.dar_like()
+atlanta.dar_like()
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-      f'- Temporadas: {atlanta.temporadas}')
+      f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
