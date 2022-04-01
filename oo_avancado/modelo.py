c74d0244ac0b48f17fb695a446a0bdbbd8b5087b
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-04-01 16:35:23 -0300

    reduzindo ifs e sobreescrevendo metodos

--- oo_avancado/modelo.py
+++ oo_avancado/modelo.py
@@ -22,15 +22,24 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self._likes}')
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.duracao} - {self._likes}')
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+    
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes}')
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -41,11 +50,7 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
-
 filmes_e_series = [vingadores, atlanta]
 
-for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f'{programa.nome} - {detalhes} D - {programa.likes}')
+#for programa in filmes_e_series:
+#    programa.imprime()
