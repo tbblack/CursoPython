bfc802881fcfdc5962fccdeda6748cf49dca497b
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-04-06 15:58:15 -0300

    magic mathods

--- oo_avancado/modelo.py
+++ oo_avancado/modelo.py
@@ -45,14 +45,17 @@ class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
         self._programas = programas
+    
+    def __getitem__(self, item):
+        return self._programas[item]
 
+    def __len__(self):
+        return len(self._programas)
+        
     @property
     def listagem(self):
         return self._programas
     
-    @property
-    def tamanho(self):
-        return len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -74,10 +77,7 @@ atlanta.dar_likes()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho do Playlist: {len(playlist_fim_de_semana.listagem)}')
+print(f'Tamanho do Playlist: {len(playlist_fim_de_semana)}')
 
-for programa in playlist_fim_de_semana.listagem:
+for programa in playlist_fim_de_semana:
     print(programa)
-
-print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana.listagem}')
-
