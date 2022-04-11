f8def6fe685dc6c4481077b6cd40b30f4798e5d8
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-04-11 18:19:31 -0300

    ajustando nova aplicação em django

---
+++ django/aplicacao/receitas/models.py
@@ -0,0 +1,13 @@
+from email.policy import default
+import imp
+from django.db import models
+from datetime import datetime
+
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField()
+    rendimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    date_receita = models.DateTimeField(default=datetime.now, blank=True)
