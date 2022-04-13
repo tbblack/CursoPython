9d6eb89da7295f5c04aa0c819e86500931e64d06
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-04-13 17:58:14 -0300

    ajustando partials

--- django/aplicacao/receitas/models.py
+++ django/aplicacao/receitas/models.py
@@ -15,3 +15,5 @@ class Receita(models.Model):
     date_receita = models.DateTimeField(default=datetime.now, blank=True)
     fot_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
     publicada = models.BooleanField(default=False)
+    def __str__(self):
+        return self.nome_receita
