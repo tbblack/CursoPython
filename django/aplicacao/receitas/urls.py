3312f850016f96f572dd0366219dcbd4a9d9298f
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-04-13 14:58:37 -0300

    Configurando para subir fotos e ajustando acesso as fotos

--- django/aplicacao/receitas/urls.py
+++ django/aplicacao/receitas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('receita/<int:receita_id>', views.receita, name='receita')
-]
+    path('receita/<int:receita_id>', views.receita, name='receita'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django/aplicacao/receitas/views.py
+++ django/aplicacao/receitas/views.py
@@ -3,7 +3,7 @@ from .models import Receita
 
 def index(request):
 
-    receitas = Receita.objects.all
+    receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
 
     dados = {
         'receitas' : receitas
