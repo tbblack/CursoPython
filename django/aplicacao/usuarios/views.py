cd5ce077db46eff8c03f5051313d211ea56ebc10
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-04-19 13:43:53 -0300

    verificando se esta autenticado para mudar a renderização

--- django/aplicacao/usuarios/views.py
+++ django/aplicacao/usuarios/views.py
@@ -55,7 +55,13 @@ def login(request):
     return render(request, 'usuarios/login.html')
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 def dashboard(request):
-    return render(request, 'usuarios/dashboard.html')
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
+
+    
