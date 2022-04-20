c171843561491dc4db20089e0b4c110abfb0894a
Thales Barbosa <thalesbarbosadeoliveira@gmail.com>
2022-04-20 17:50:05 -0300

    refatorando codigo, modularizando views

--- django/aplicacao/usuarios/views.py
+++ django/aplicacao/usuarios/views.py
@@ -3,12 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from receitas.models import Receita
 
-def campo_vazio(campo):
-    return not campo.strip()
-
-def senhas_nao_sao_iguais(senha, senha2):
-    return senha != senha2
-
 def cadastro(request):
 
     if request.method == 'POST':
@@ -82,55 +76,8 @@ def dashboard(request):
     else:
         return redirect('index')
 
-def cria_receita(request):
-    
-    if request.method == 'POST':
-        nome_receita = request.POST['nome_receita']
-        ingredientes = request.POST['ingredientes']
-        modo_preparo = request.POST['modo_preparo']
-        tempo_preparo = request.POST['tempo_preparo']
-        rendimento = request.POST['rendimento']
-        categoria = request.POST['categoria']
-        foto_receita = request.FILES['foto_receita']
-        user = get_object_or_404(User, pk=request.user.id)
+def campo_vazio(campo):
+    return not campo.strip()
 
-        receita = Receita.objects.create(pessoa=user, 
-        nome_receita=nome_receita, ingredientes=ingredientes, 
-        modo_preparo=modo_preparo, tempo_preparo=tempo_preparo, 
-        rendimento=rendimento, categoria=categoria, 
-        fot_receita=foto_receita)
-
-        receita.save()
-
-        return redirect('dashboard')
-    else:
-        return render(request, 'usuarios/cria_receita.html')   
-
-def deleta_receita(request, receita_id):
-    receita = get_object_or_404(Receita, pk=receita_id)
-    receita.delete()
-    return redirect('dashboard')
-
-def edita_receita(request, receita_id):
-    receita = get_object_or_404(Receita, pk=receita_id)
-    receita_a_editar = { 'receita' : receita }
-    return render(request, 'usuarios/edita_receita.html', receita_a_editar)
-
-def atualiza_receita(request):
-    if request.method == 'POST':
-
-        r = Receita.objects.get(pk=request.POST['receita_id'])
-
-        r.nome_receita=request.POST['nome_receita'] 
-        r.ingredientes=request.POST['ingredientes']
-        r.modo_preparo=request.POST['modo_preparo'] 
-        r.tempo_preparo=request.POST['tempo_preparo'] 
-        r.rendimento=request.POST['rendimento'] 
-        r.categoria=request.POST['categoria']
-
-        if 'foto_receita' in request.FILES:
-            r.fot_receita=request.FILES['foto_receita']
-
-        r.save()
-
-        return redirect('dashboard')
+def senhas_nao_sao_iguais(senha, senha2):
+    return senha != senha2
